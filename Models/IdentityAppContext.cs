07a81f0f0ed7a732f7f04266ea12f527cadb52ba
unknown <maceo4658@gmail.com>
2021-05-04 21:04:12 -0500

    First commit. Setup communication with MySql

---
+++ Models/IdentityAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MyWeaponList.Models
+{
+    public class IdentityAppContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public IdentityAppContext(DbContextOptions<IdentityAppContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
